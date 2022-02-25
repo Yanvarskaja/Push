c83052596c7b344121de32ee4b38af20573d0043
Viktotija Popovskaja <yanvarskaja89@gmail.com>
2022-02-25 19:11:51 +0800

    Исправлены замечания push

--- app/src/main/java/ru/netology/nmedia/service/FCMService.kt
+++ app/src/main/java/ru/netology/nmedia/service/FCMService.kt
@@ -6,12 +6,15 @@ import android.content.Context
 import android.os.Build
 import android.util.Log
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.google.gson.Gson
 import com.google.gson.JsonSyntaxException
 import ru.netology.nmedia.R
 import ru.netology.nmedia.auth.AppAuth
+import java.util.*
+import kotlin.random.Random
 
 class FCMService : FirebaseMessagingService() {
     private val content = "content"
@@ -40,7 +43,6 @@ class FCMService : FirebaseMessagingService() {
                 AppAuth.getInstance().authStateFlow.value.id, null -> handlePush(push)
                 else -> AppAuth.getInstance().sendPushToken()
             }
-            handlePush(push)
         } catch (e: JsonSyntaxException) {
             Log.e("FSMService", "can't parse :${message.data["content"]}")
         }
@@ -51,6 +53,9 @@ class FCMService : FirebaseMessagingService() {
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             .setContentText(push.content)
             .build()
+
+        NotificationManagerCompat.from(this)
+            .notify(Random.nextInt(100_000), notification)
     }
 
     override fun onNewToken(token: String) {
